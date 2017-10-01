258909678aeef9eeef2a7260d29c2a3c87778e41
Siarhei Blashuk <siarhei_blashuk@epam.com>
2017-10-01 15:34:01 +0300

    added discount service

--- src/main/java/com/epam/cinema/service/DiscountService.java
+++ src/main/java/com/epam/cinema/service/DiscountService.java
@@ -6,5 +6,5 @@ import com.epam.cinema.model.User;
 import java.time.LocalDateTime;
 
 public interface DiscountService {
-    byte getDiscount(User user, Event event, LocalDateTime airDateTime, long numberOfTickets);
+    Double getDiscount(Event event, User user, LocalDateTime airDateTime, Long numberOfTickets);
 }
---
+++ src/main/java/com/epam/cinema/service/DiscountServiceImpl.java
@@ -0,0 +1,27 @@
+package com.epam.cinema.service;
+
+import com.epam.cinema.discount.DiscountStrategy;
+import com.epam.cinema.model.Event;
+import com.epam.cinema.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class DiscountServiceImpl implements DiscountService {
+
+    List<DiscountStrategy> discountStrategyList;
+
+    public DiscountServiceImpl(List<DiscountStrategy> discountStrategyList) {
+        this.discountStrategyList = discountStrategyList;
+    }
+
+
+    @Override
+    public Double getDiscount(Event event, User user, LocalDateTime airDateTime, Long numberOfTickets) {
+        return discountStrategyList
+                .stream()
+                .map(discountStrategy -> discountStrategy.calculate(event, user, airDateTime, numberOfTickets))
+                .max(Double::compare)
+                .orElse(0.0);
+    }
+}
