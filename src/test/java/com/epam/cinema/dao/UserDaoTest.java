c5a5d7ce363a203d991688508e1f35bae71f83c6
Siarhei Blashuk <siarhei_blashuk@epam.com>
2017-10-15 18:20:53 +0300

    fixed tests

--- src/test/java/com/epam/cinema/dao/UserDaoTest.java
+++ src/test/java/com/epam/cinema/dao/UserDaoTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Objects;
@@ -19,6 +20,7 @@ import static java.util.Objects.isNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=SpringTestConfiguration.class)
+@Transactional
 public class UserDaoTest {
 
     @Autowired
